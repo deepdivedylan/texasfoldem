1cb471daf35f649c9e476a3b541a305bbed34453
Dylan McDonald <dmcdonald21@cnm.edu>
2017-04-30 19:54:20 -0600

    added shuffle to deck

--- src/main/java/io/deepdivedylan/texasfoldem/Deck.java
+++ src/main/java/io/deepdivedylan/texasfoldem/Deck.java
@@ -1,14 +1,39 @@
 package io.deepdivedylan.texasfoldem;
 
+import org.apache.commons.math3.random.MersenneTwister;
+
 public class Deck {
 	private Card[] deck = null;
 
 	public Deck() {
 		this.deck = new Card[52];
+		Integer i = 0;
 		for(Suit suit : Suit.values()) {
 			for(Rank rank : Rank.values()) {
-				
+				this.deck[i] = new Card(rank, suit);
+				i++;
 			}
 		}
+	}
+
+	public void shuffle() {
+		MersenneTwister mersenneTwister = new MersenneTwister();
+		Integer i = 0, length = this.deck.length;
+
+		for(i = length - 1; i > 0; i++) {
+			Integer j = mersenneTwister.nextInt(length);
+			Card temp = this.deck[i];
+			this.deck[i] = this.deck[j];
+			this.deck[j] = temp;
+		}
+	}
+
+	public String toString() {
+		String output = "";
+		String newline = System.lineSeparator();
+		for(Card card : this.deck) {
+			output = card + newline;
+		}
+		return(output);
 	}
 }
